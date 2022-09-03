2895a7b26a32d74a062fd1c528f89048f36324da
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-09-03 09:41:51 +0600

    item-found-section-uploaded

--- js/news.js
+++ js/news.js
@@ -13,10 +13,33 @@ const displayCategories = (categories) => {
         const createDiv = document.createElement('div');
         createDiv.classList.add('category-all', 'mx-auto')
         createDiv.innerHTML = `
-        <a href="">${category.category_name}</a>
+        <p id="item-mouse-style" onclick="loadItemsFound('${category.category_id}','${category.category_name}')">${category.category_name}</p>
         `;
         callCategoriesContainer.appendChild(createDiv);
     })
 
 }
+const loadItemsFound = (categoryId, categoryName) => {
+    // console.log(categoryId);
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayItemFound(data.data, categoryName))
+        .catch(error => console.log(error))
+}
+const displayItemFound = (categories, categoryName) => {
+    // console.log(categoryName);
+    const itemsFoundContainer = document.getElementById('items-found-container');
+    itemsFoundContainer.innerText = `${categories.length} items found  for category ${categoryName}
+    `
+}
+
+
+
+
+
+
+
+
+
 loadCategories();
